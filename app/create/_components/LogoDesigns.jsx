884247b1c390d6db8ccf329ecc8f9dea29cd7c4c
Aditya <147301021+adityajha2005@users.noreply.github.com>
2025-01-13 10:14:42 +0530

    created logo design page

--- app/create/_components/LogoDesigns.jsx
+++ app/create/_components/LogoDesigns.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Image from 'next/image';
+import HeadingDescription from './HeadingDescription';
+import Lookup from '@/app/_data/Lookup';
+import LogoDesig from '@/app/_data/LogoDesig';
 
-const LogoDesigns = () => {
+function LogoDesigns({ onHandleInputChange }) {
+  const [selectedOption, setSelectedOption] = useState();
+
+  const handleClick = (design) => {
+    setSelectedOption(design.title);
+    onHandleInputChange(design); 
+  };
+
   return (
-    <div>LogoDesigns</div>
-  )
+    <div className='my-10'>
+      <HeadingDescription
+        title={Lookup.LogoDesignTitle}
+        description={Lookup.LogoDesignDesc}
+      />
+      <div className='grid grid-cols-2 md:grid-cols-3 gap-10 mt-10'>
+        {LogoDesig.map((design, index) => (
+          <div
+            key={index}
+            onClick={() => handleClick(design)} 
+            className={`p-1 cursor-pointer hover:border-2 border-primary rounded-xl ${selectedOption === design.title && 'border rounded-xl border-primary'}`}
+          >
+            <Image
+              src={design.image}
+              alt={design.title}
+              width={300}
+              height={200}
+              className='w-full rounded-xl h-[150px] object-cover'
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default LogoDesigns
+export default LogoDesigns;
