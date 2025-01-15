719d8b867c8069e98bcea79ce25300b23617b7db
Aditya <147301021+adityajha2005@users.noreply.github.com>
2025-01-15 19:23:11 +0530

    logoIdea finally updated

--- app/create/_components/LogoIdea.jsx
+++ app/create/_components/LogoIdea.jsx
@@ -1,37 +1,67 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import HeadingDescription from './HeadingDescription'
 import Lookup from '@/app/_data/Lookup'
-import LogoDesig from '@/app/_data/LogoDesig'
-import Image from 'next/image'
+import axios from 'axios'
+import Prompt from '@/app/_data/Prompt'
+import { Loader2Icon } from 'lucide-react'
 
-function LogoDesigns({onHandleInputChange,formData}) {
-  const [selectedOption,setSelectedOption]=useState(formData?.design?.title);
+function LogoIdea({formData,onHandleInputChange}) {
+
+  const [ideas,setIdeas]=useState();
+  const [loading,setLoading]=useState(false);
+  const [selectedOption,setSelectedOption]=useState(formData?.idea);
+  useEffect(()=>{
+    generateLogoDesignIdea();
+  },[])
+
+  const generateLogoDesignIdea=async()=>{
+   
+    setLoading(true)
+    const PROMPT=Prompt.DESIGN_IDEA_PROMPT
+    .replace('{logoType}',formData?.design.title)
+    .replace('{logoTitle}',formData.title)
+    .replace('{logoDesc}',formData.desc)
+    .replace('{logoPrompt}',formData.design.prompt)
+
+    // console.log(PROMPT);
+    const result=await axios.post('/api/ai-design-ideas',{
+      prompt:PROMPT
+    })
+
+    console.log(result.data)
+   !ideas&&setIdeas(result.data.ideas);
+    setLoading(false);
+  }
+
   return (
     <div className='my-10'>
       <HeadingDescription
-      title={Lookup.LogoDesignTitle}
-      description={Lookup.LogoDesignDesc}
+      title={Lookup.LogoIdeaTitle}
+      description={Lookup.LogoIdeaDesc}
       />
-
-      <div className='grid grid-cols-2 md:grid-cols-3 gap-10 mt-10'>
-        {LogoDesig.map((design,index)=>(
-          <div key={index} 
-          onClick={()=>{setSelectedOption(design.title);
-            onHandleInputChange(design)
-          }}
-          
-          className={`p-1 hover:border-2 border-primary rounded-xl cursor-pointer ${selectedOption==design.title&&'border-2 rounded-xl border-primary'}`}>
-              <Image src={design.image} alt={design.title} width={300}
-               height={200}
-               className='w-full rounded-xl h-[150px] object-cover'
-               />
-
-               <h2 className='text-center mt-3 font-medium text-lg'>{design.title}</h2>
-          </div>
-        ))}
-      </div>
+    <div className='flex items-center justify-center'>
+    {loading&&<Loader2Icon className='animate-spin my-10' />}
     </div>
+    <div className='flex flex-wrap gap-3 mt-6'>
+      {ideas&&ideas.map((item,index)=>(
+        <h2 key={index}
+        onClick={()=>{setSelectedOption(item);
+          onHandleInputChange(item)
+        }}
+        className={`p-2 rounded-full border px-3 cursor-pointer
+          hover:border-primary ${selectedOption==item&&'border-primary'}`}
+        >{item}</h2>
+      ))}
+      <h2 
+       onClick={()=>{setSelectedOption('Let AI Select the best idea');
+        onHandleInputChange('Let AI Select the best idea')
+      }}
+      className={`p-2 rounded-full border px-3 cursor-pointer
+          hover:border-primary ${selectedOption=='Let AI Select the best idea'&&'border-primary'}`}>Let AI Select the best idea</h2>
+    </div>
+    </div>
+    
   )
 }
 
-export default LogoDesigns
+export default LogoIdea
